deca650d5e5bd0f7abc1a66b75b85ea029702928
ichiragkumar <chiragroy3030@gmail.com>
2025-05-07 23:44:41 +0530

    read about sub- resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { TodoModule } from './todo/todo.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [HelloModule],
+  imports: [HelloModule, TodoModule, MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
